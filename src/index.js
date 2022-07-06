090a5659f7752a20755217639ac68dcbeb747cb7
Himal Gautam <himalpgautam@gmail.com>
2022-07-06 17:00:17 +0530

    .

--- src/index.js
+++ src/index.js
@@ -1,32 +1,31 @@
-import express from 'express'
-import './db/mongoose.js'
-import userRouter from './Routers/user.js'
-import dataRouter from './Routers/data.js'
-import cors from 'cors'
+import express from "express";
+import "./db/mongoose.js";
+import userRouter from "./Routers/user.js";
+import dataRouter from "./Routers/data.js";
+import cors from "cors";
 import chalk from "chalk";
 
-const app = express()
-const port = process.env.PORT
+const app = express();
+const port = process.env.PORT;
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.json())
-app.use(userRouter)
-app.use(dataRouter)
+app.use(express.json());
+app.use(userRouter);
+app.use(dataRouter);
 
 app.get("/", (req, res) => {
-  console.log('hi');
-    res.send("Welcome to Markdown Viewer Server !!!")
-})
+  console.log("hi");
+  res.send("Welcome to Markdown Viewer Server !!!");
+});
 
 app.listen(port, () => {
-    console.log(
-      chalk.magenta.bold.underline("Server is up on port ") +
-        chalk.blue.bold.underline(port)
-    );
-  });
-  
-  app.get("/", function (request, response) {
-    response.send("Welcome to NCSC Project Server");
-  });
-  
+  console.log(
+    chalk.magenta.bold.underline("Server is up on port ") +
+      chalk.blue.bold.underline(port)
+  );
+});
+
+app.get("/", function (request, response) {
+  response.send("Welcome to NCSC Project Server");
+});
